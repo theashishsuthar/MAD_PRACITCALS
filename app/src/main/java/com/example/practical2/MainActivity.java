2b55d19a35d0d6ef16570b637b93b989c7079256
Ashish <sutharashish5656@gmail.com>
2021-10-09 10:02:39 +0530

    Practical1

---
+++ app/src/main/java/com/example/practical2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.practical2;
+
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText email,password,userName;
+    Button login;
+    CheckBox rememberMe;
+    /////////////////////
+
+
+}
